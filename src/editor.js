a052ab6e973f51e885520a3dbc00a3d0b4d67dcd
K Adam White <kadamwhite@users.noreply.github.com>
2021-02-03 00:01:43 -0500

    Add a second post lock based on category to test lock interaction

--- src/editor.js
+++ src/editor.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { registerPlugin } from '@wordpress/plugins';
 
@@ -10,7 +10,7 @@ const LockPostIfNoTagsPlugin = () => {
     const { lockPostSaving, unlockPostSaving } = useDispatch( 'core/editor' );
 
     useEffect( () => {
-        console.log( 'Checking: ' + tags.length + '; ' + ( isLocked ? 'true' : 'false' ) );
+        console.log( 'Checking Tags: ' + tags.length + '; ' + ( isLocked ? 'true' : 'false' ) );
         if ( ! isLocked && tags.length === 0 ) {
             lockPostSaving( 'no-tags' );
             setLock( true );
@@ -24,6 +24,39 @@ const LockPostIfNoTagsPlugin = () => {
     return null;
 };
 
+
+const LockPostIfNoCatsPlugin = () => {
+    const [ isLocked, setLock ] = useState( false );
+    const {
+        categories,
+        lock,
+    } = useSelect( ( select ) => ( {
+        categories: select( 'core/editor' ).getEditedPostAttribute( 'categories' ),
+        lock: select( 'core/editor' ).getActivePostLock(),
+    } ) );
+    const { lockPostSaving, unlockPostSaving } = useDispatch( 'core/editor' );
+
+    useEffect( () => {
+        console.log( 'Active Lock: ', lock );
+        console.log( 'Checking Cats: ' + categories.length + '; ' + ( isLocked ? 'true' : 'false' ) );
+        if ( ! isLocked && categories.length === 0 ) {
+            lockPostSaving( 'no-categories' );
+            setLock( true );
+        } else if ( isLocked && categories.length > 0 ) {
+            unlockPostSaving( 'no-categories' );
+            setLock( false );
+        }
+    }, [ isLocked, categories.length, lockPostSaving, unlockPostSaving ] );
+
+    // We're using this component only to plug into the React digest cycle, no rendering is needed.
+    return null;
+};
+
 registerPlugin( 'test-plugin', {
-    render: LockPostIfNoTagsPlugin,
+    render: () => (
+        <Fragment>
+            <LockPostIfNoTagsPlugin />
+            <LockPostIfNoCatsPlugin />
+        </Fragment>
+    ),
 } );
